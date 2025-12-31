6ac915bcf52ac85c3fc54e0118fbd7c7ad80a565
peacecode14-alt <peacecode14@gmail.com>
2025-12-31 21:01:18 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -4,3 +4,27 @@ function goTo(pageId) {
   });
   document.getElementById(pageId).classList.add('active');
 }
+
+function uploadVideo() {
+  const pass = document.getElementById("teacherPass").value;
+  const fileInput = document.getElementById("videoInput");
+  const video = document.getElementById("lessonVideo");
+
+  if (pass !== "2025") {
+    alert("كلمة السر غير صحيحة");
+    return;
+  }
+
+  if (fileInput.files.length === 0) {
+    alert("من فضلك اختر فيديو");
+    return;
+  }
+
+  const file = fileInput.files[0];
+  const videoURL = URL.createObjectURL(file);
+
+  video.src = videoURL;
+  video.style.display = "block";
+
+  alert("تم رفع الفيديو بنجاح");
+}
